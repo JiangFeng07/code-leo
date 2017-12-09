f1a9cb6d60abb67cfab0295cedcfb52d79cc29c4
jiangfeng07 <jiangfeng07@meituan.com>
2017-12-09 13:40:01 +0800

    modify

--- src/main/java/com/leo/leetcode/MergeTwoBinaryTrees_617.java
+++ src/main/java/com/leo/leetcode/MergeTwoBinaryTrees_617.java
@@ -11,8 +11,6 @@ public class MergeTwoBinaryTrees_617 {
         if (t2 == null) {
             return t1;
         }
-//        TreeNode tmp_t1 = t1;
-//        TreeNode tmp_t2 = t2;
         int value = t1.getVal() + t2.getVal();
         TreeNode t = new TreeNode(value);
         t.setLeft(mergeTrees(t1.getLeft(), t2.getLeft()));
