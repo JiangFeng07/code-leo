df3d5d5c2dda6f5d376b4a34d76abe7453bc49a1
jiangfeng07 <jiangfeng07@meituan.com>
2018-01-07 16:45:12 +0800

    modify

---
+++ src/main/java/com/leo/leetcode/MinimumASCIIDeleteSumForTwoStrings_712.java
@@ -0,0 +1,10 @@
+package com.leo.leetcode;
+
+/**
+ * Created by lionel on 18/1/7.
+ */
+public class MinimumASCIIDeleteSumForTwoStrings_712 {
+    public static void main(String[] args) {
+        System.out.println(Integer.valueOf('s'));
+    }
+}
